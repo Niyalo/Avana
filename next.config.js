71acbfe1f698482c324fea3056fa86bab81ffb3c
Ad1-tya <076bei003.aditya@pcampus.edu.np>
2023-09-18 11:41:50 +0545

    changes in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/Niyalo/Avana/' : '',
     output: "export"
 }
 
-module.exports = {
-    nextConfig,
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/Niyalo/Avana/' : '',
-}
+module.exports = nextConfig
