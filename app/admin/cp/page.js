aefce12785c615bece412c6ee5bf6f454b139b2b
Ad1-tya <076bei003.aditya@pcampus.edu.np>
2023-09-18 13:28:48 +0545

    added sections in create-project page

--- app/admin/cp/page.js
+++ app/admin/cp/page.js
@@ -18,25 +18,32 @@ const cp = () => {
             <input type="text" placeholder='Enter Project Title' className='text-base bg-white50 text-black150 focus:outline-none border-b p-2 w-full'/>
         </div>
         <div className='flex flex-col gap-2 h-max'>
-            <h4 className='text-primary'>Upload Project Image</h4>
-            <input type="file" accept="image/*" placeholder='Upload Project Image' className='text-base h-[100px] text-black150 border-none file:w-full file:h-full file:cursor-pointer file:bg-white100  file:m-0 file:mt-1 file:text-black150 file:px-3 file:border-none'/>
-        </div>
-        <div className='flex flex-col gap-2 h-max'>
-            <h4 className='text-primary'>Project Description</h4>
-            <textarea
-                className="h-[200px] rounded-sm p-3 w-full focus:outline-none"
-                placeholder='Add Description of Project'
-                rows={20}
-                cols={80} 
-            />
-        </div>
-        <div className='flex flex-col gap-2 h-max'>
             <h4 className='text-primary'>Project Deadline</h4>
             <div className='flex gap-8 w-full'>
               <label className='flex w-full whitespace-nowrap items-center gap-4 text-primary'>Start Date: <input type="date" placeholder='Start Date' className='text-base bg-white100 text-black150 rounded-sm focus:outline-none p-2 px-4 w-max'/></label>
               <label className='flex w-full whitespace-nowrap items-center gap-4 text-primary'>End Date: <input type="date" placeholder='End Date' className='text-base bg-white100 text-black150 rounded-sm focus:outline-none p-2 px-4 w-max'/></label>
             </div>
         </div>
+        <h4 className='text-primary'>Project Sections:</h4>
+        <div className='flex flex-col gap-4'>
+          <div className='flex flex-col gap-2 h-max'>
+              <input type="text" placeholder='Section title (Optional)' className='text-base bg-white50 text-black150 focus:outline-none border-b p-2 w-full'/>
+          </div>
+          <div className='flex flex-col gap-2 h-max'>
+              <h4 className='text-primary'>Upload Section Image</h4>
+              <input type="file" accept="image/*" placeholder='Upload Project Image' className='text-base h-[100px] text-black150 border-none file:w-full file:h-full file:cursor-pointer file:bg-white100  file:m-0 file:mt-1 file:text-black150 file:px-3 file:border-none'/>
+          </div>
+          <div className='flex flex-col gap-2 h-max'>
+              <h4 className='text-primary'>Section Description</h4>
+              <textarea
+                  className="h-[200px] rounded-sm p-3 w-full focus:outline-none"
+                  placeholder='Add Description of Project'
+                  rows={20}
+                  cols={80} 
+              />
+          </div>
+        </div>
+        <Button type="add" label="Add Sections" />
       </div> 
     </div>
   )
